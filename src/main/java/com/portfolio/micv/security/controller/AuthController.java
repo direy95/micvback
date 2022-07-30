0c920d5ef5a65cc924fb0d852afbf7306f330188
Diego Rey <holacinema@hotmail.com>
2022-07-30 18:27:28 -0300

    Eliminado cors de auth

--- src/main/java/com/portfolio/micv/security/controller/AuthController.java
+++ src/main/java/com/portfolio/micv/security/controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+//@CrossOrigin
 public class AuthController {
     @Autowired PasswordEncoder passwordEncoder;
     @Autowired AuthenticationManager authenticationManager;
